42e8f8c8317e98f197ea68b8684b112ff1219fa0
Annan Zia <annanzia13@gmail.com>
2026-02-19 00:35:41 -0600

    added gitignore and grocerymanager.java to main branch with initialized code

---
+++ GroceryManager.java
@@ -0,0 +1,7 @@
+public class GroceryManager {
+    public static void main(String[] args) {
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+    }
+}
