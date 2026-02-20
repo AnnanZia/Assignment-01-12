3fa671ed60908963142b115bab505f31e2cea68e
Annan Zia <annanzia13@gmail.com>
2026-02-20 04:07:55 -0600

    Added javadoc

--- GroceryManager.java
+++ GroceryManager.java
@@ -8,7 +8,52 @@ public class GroceryManager {
         String[] itemNames = new String[10];
         double[] itemPrices = new double[10];
         int[] itemStocks = new int[10];
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1) View Inventory");
+            System.out.println("2) Restock Item");
+            System.out.println("3) Exit");
+            System.out.print("Choice: ");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            if (choice == 1) {
+                printInventory(itemNames, itemPrices, itemStocks);
+            } else if (choice == 2) {
+                System.out.print("Enter item name: ");
+                String target = sc.nextLine();
+
+                System.out.print("Enter amount: ");
+                int amount = sc.nextInt();
+                sc.nextLine();
+
+                restockItem(itemNames, itemStocks, target, amount);
+            } else if (choice == 3) {
+                break;
+            }
+        }
+
+        sc.close();
     }
+
+    /**
+    * Prints the inventory of grocery items, including their names, prices, and how many are in stock.
+
+    * @param names An array of item names.
+    * @param prices An array of item prices corresponding to the names.
+    * @param stocks An array of item stock quantities corresponding to the names.
+    */
+    public static void printInventory(String[] names, double[] prices, int[] stocks) {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] != null) {
+                System.out.println(names[i] + " $" + prices[i] + " stock=" + stocks[i]);
+            }
+        }
+    }
+
     /**
      * Increases the stock quantity of an existing grocery item.
      * <p>
